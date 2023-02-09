af7a7e057eeaae63c8881f75932e556fb20a4ff2
Joao Gabriel Gouveia De Souza Brito <gabriel.gouveia@live.com>
2023-02-09 00:36:39 -0300

    Delete src directory

--- src/excecao/CidadaoException.java
+++
@@ -1,9 +0,0 @@
-package excecao;
-
-public class CidadaoException extends Exception{
-
-	public CidadaoException(String e)
-	{
-		super(e);
-	}
-}
--- src/excecao/EntidadeException.java
+++
@@ -1,9 +0,0 @@
-package excecao;
-
-public class EntidadeException extends Exception{
-
-	public EntidadeException(String e)
-	{
-		super(e);
-	}
-}
